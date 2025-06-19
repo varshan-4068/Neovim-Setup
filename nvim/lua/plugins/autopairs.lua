b700cd1c52a8b206b3b69f8e2db293c36a0a670b
Sirivarshan K <j42830963@gmail.com>
2025-06-19 22:32:38 +0530

    Neovim With Linting and formatting and completions

---
+++ nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,15 @@
+return{
+		'windwp/nvim-autopairs',
+		config = function ()
+			require('nvim-autopairs').setup({
+				check_ts = true,
+				map_cr = true,
+				map_bs = true,
+
+				chars = {'{','[','(','"',"'"},
+				pattern = string.gsub([[ [%'%"%(%{%[] ]], '%s+', ''),
+				offset = 0,
+				end_key = '$'
+			})
+		end
+}
---
+++ nvim/lua/plugins/formatting.lua
@@ -0,0 +1,24 @@
+return{
+	"stevearc/conform.nvim",
+	config = function ()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort","black" },
+				javascript = { "prettierd", "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				typescript = { "prettier" },
+				cpp = { "clang-format" },
+			},
+		})
+
+		vim.keymap.set({ "n", "v"}, "<leader>f" ,function ()
+			require("conform").format({
+			lsp_fallback = true,
+			async = true,
+			})
+		end)
+	end
+}
