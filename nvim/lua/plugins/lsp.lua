bc70b17275090813fe3f3e9ceb572297ee4f156d
Sirivarshan K <j42830963@gmail.com>
2025-06-26 11:32:35 +0530

    Added bashls to lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -17,7 +17,7 @@ return{
   	"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = {"lua_ls", "pyright", "cssls", "html", "ts_ls","clangd"}
+				ensure_installed = {"lua_ls", "pyright", "cssls", "html", "ts_ls","clangd","bashls"}
 			})
 		end
 	},
