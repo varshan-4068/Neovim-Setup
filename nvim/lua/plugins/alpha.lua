1e1c6b9463a37d60a19cc95377573a35be0cdb24
Sirivarshan K <j42830963@gmail.com>
2025-07-08 19:38:53 +0530

    Updates..

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -6,25 +6,18 @@ return {
 
   config = function()
     local alpha = require 'alpha'
-    local dashboard = require 'alpha.themes.dashboard'
+    local dashboard = require 'alpha.themes.startify'
 
     dashboard.section.header.val = {
-      [[ -----------------MADE BY SIRIVARSHAN---------------- ]],
-      [[  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗  ]],
-      [[  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║  ]],
-      [[  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║  ]],
-      [[  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║  ]],
-      [[  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║  ]],
-      [[  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝  ]],
-      [[ -------------------I USE ARCH BTW------------------- ]],
+      [[																			 -----------------MADE BY SIRIVARSHAN---------------- ]],
+      [[																				███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗  ]],
+      [[																				████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║  ]],
+      [[																				██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║  ]],
+      [[																				██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║  ]],
+      [[																				██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║  ]],
+      [[																				╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝  ]],
+      [[																			 -------------------I USE ARCH BTW------------------- ]],
     }
-
-		dashboard.section.buttons.val = {
-			dashboard.button("ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("fw", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-		  dashboard.button( "n", "  > New File" , ":ene <BAR> startinsert <CR>"),
-			dashboard.button("q", "  > Quit Nvim", "<cmd>qa<CR>"),
-		}
 
 	  vim.cmd[[autocmd User AlphaReady echo 'Ready']]
 
