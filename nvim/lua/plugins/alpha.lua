b18015ee85d563319ea3306ab6ce1f51e628a7ea
Sirivarshan K <j42830963@gmail.com>
2025-10-27 22:40:25 +0530

    Updates..

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -22,7 +22,6 @@ return {
 				"⣿⣿⣿⣿⣿⣿⠿⠁⠀⠀⠀⠘⣤⣭⣟⠛⠛⣉⣁⡜⠀⠀⠀⠀⠀⠛⠿⣿⣿⣿",
 				"⡿⠟⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⡀⠀⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉",
 				" 														 ",
-				"         Sirivarshan K        ",
 		}
 
 		dashboard.section.buttons.val = {
